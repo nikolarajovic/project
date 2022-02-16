16edaa8e8121f1f3975fa61234fb41234670beaa
Nikola Rajovic <nikola.rajovic1992@gmail.com>
2022-02-16 15:32:22 +0100

    BasicTest update

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -16,26 +16,33 @@ import sun.net.www.protocol.http.AuthCacheImpl;
 
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 
-public class BasicTest {
+public abstract class BasicTest {
 
-	private WebDriver driver;
-	private WebDriverWait wait;
-	private LocationPopupPage locationPopupPage;
-	private LoginPage loginPage;
-	private NotificationSystemPage notificationSystemPage;
-	private ProfilePage profilePage;
-	private AuthPage authPage;
-	private MealPage mealPage;
-	private CartSummaryPage cartSummaryPage;
-	private SearchResultPage searchResultPage;
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	protected LocationPopupPage locationPopupPage;
+	protected LoginPage loginPage;
+	protected NotificationSystemPage notificationSystemPage;
+	protected ProfilePage profilePage;
+	protected AuthPage authPage;
+	protected MealPage mealPage;
+	protected CartSummaryPage cartSummaryPage;
+	protected SearchResultPage searchResultPage;
 
 	@BeforeMethod
 	public void beforeMethod() {
@@ -61,10 +68,10 @@ public class BasicTest {
 	@Test
 	public void function() throws InterruptedException {
 
-		locationPopupPage.chooseLocation("Beverwyck - Albany");
-		loginPage.login("customer@dummyid.com", "12345678a");
-		notificationSystemPage.waitForMessageToDisappear();
-//		profilePage.goToProfilePage();
+//		locationPopupPage.chooseLocation("Beverwyck - Albany");
+//		loginPage.login("customer@dummyid.com", "12345678a");
+//		notificationSystemPage.waitForMessageToDisappear();
+////		profilePage.goToProfilePage();
 //		profilePage.uploadPhoto("img/profilePhoto.jpeg");
 //		profilePage.removePhoto();
 //		profilePage.personalInformationInput("Jackson", "Roland", "StreetInUK10", "066666666", "18000",
@@ -73,13 +80,26 @@ public class BasicTest {
 //		mealPage.addToFavourite();
 //		mealPage.addProductToTheCart("3");
 //		cartSummaryPage.clearAll();
-		searchResultPage.searchResultsNames();
+//		searchResultPage.searchResultsNames();
+		
+//		[ERROR] Unexpected logout message
+//		rešenje sa listom ili try/catch-om
+		
+		String email = "customer@dummyid.com";
+		String password = "12345678a";
 
 	}
 
 	@AfterMethod
-	public void afterMethod() {
-//		driver.quit();
+	public void afterMethod() throws IOException {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy-h-mm-ss a");
+		String formattedDate = sdf.format(date);
+		TakesScreenshot screenshot = ((TakesScreenshot) driver);
+		File sourceFile = screenshot.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(sourceFile, new File("./screenshots/" + formattedDate + ".png"));
+		
+		driver.quit();
 	}
 
 }
